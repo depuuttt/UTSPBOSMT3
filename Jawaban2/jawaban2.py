cf02dc35853a2c7aa2e7646f5c5f2746a16a1eb5
depuuttt <aldimaulana023@gmail.com>
2022-11-08 09:41:16 +0700

    Commit Jawaban 2 (2)

---
+++ Jawaban2/jawaban2.py
@@ -0,0 +1,9 @@
+print("====================")
+print("PROGRAM QUIZ")
+print("====================")
+
+nama = "Aldi Maulana Iqbal"
+nim = "20210801222"
+
+print("Nama = " + nama)
+print("NIM  = " + nim)
