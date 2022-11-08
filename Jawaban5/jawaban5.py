19f105d73c68e6ba336245f32e42c065ea7480b1
depuuttt <aldimaulana023@gmail.com>
2022-11-08 11:13:34 +0700

    Jawaban 5

---
+++ Jawaban5/jawaban5.py
@@ -0,0 +1,38 @@
+import sys
+
+
+class Time:
+    def __init__(self, hours, minutes, seconds):
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
+
+    def limitFunc(self):
+        if self.hours > 24:
+            print("Hours can not go past 24")
+            print("Please try again. ^_^")
+            return False
+        if self.minutes > 60:
+            print("Minutes can not go past 60")
+            print("Please try again. ^_^")
+            return False
+        if self.seconds > 60:
+            print("Seconds can not go past 60")
+            print("Please try again. ^_^")
+            return False
+
+    def result(self):
+        print(str(self.hours) + " : " +
+              str(self.minutes) + " : " + str(self.seconds))
+
+
+hours = input("Masukkan Jam   : ")
+minutes = input("Masukkan Menit : ")
+seconds = input("Masukkan Detik : ")
+
+time = Time(int(hours), int(minutes), int(seconds))
+
+if not time.limitFunc():
+    sys.exit()
+
+time.result()
